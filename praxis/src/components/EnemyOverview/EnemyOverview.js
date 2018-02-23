781ce042d206692df6e40bedd637569d1462e8af
Andrés Felipe Aguilar Rendón <afaguilarr@unal.edu.co>
2018-02-22 21:17:27 -0500

    AHORA SI TERMINÉ
    JOJO

--- praxis/src/components/EnemyOverview/EnemyOverview.js
+++ praxis/src/components/EnemyOverview/EnemyOverview.js
@@ -5,9 +5,8 @@ class EnemyOverview extends Component {
     render() {
         return (<div className='enemy-overview-container'>
             <img className='enemy-overview-image' src={this.props.image} alt={this.props.name} />
-            <h1>{this.props.name}</h1>
+            <a href={this.props.link}><h1>{this.props.name}</h1></a>
             <p className='enemy-overview-text'>{this.props.overview}</p>
-            <a href={this.props.link}>More info...</a>
         </div>);
     }
 }
